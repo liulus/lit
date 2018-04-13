9b7bb0415f0ebcfb135c88462657186a971a3a8d
liul <liul@ipampas.com>
2018-04-13 09:45:13 +0800

    新增validate分组

---
+++ lit-commons/src/main/java/com/github/lit/commons/util/group/Group1.java
@@ -0,0 +1,9 @@
+package com.github.lit.commons.util.group;
+
+/**
+ * User : liulu
+ * Date : 2018/4/12 19:51
+ * version $Id: Group1.java, v 0.1 Exp $
+ */
+public interface Group1 {
+}
---
+++ lit-commons/src/main/java/com/github/lit/commons/util/group/Group3.java
@@ -0,0 +1,9 @@
+package com.github.lit.commons.util.group;
+
+/**
+ * User : liulu
+ * Date : 2018/4/12 19:52
+ * version $Id: Group3.java, v 0.1 Exp $
+ */
+public interface Group3 {
+}
